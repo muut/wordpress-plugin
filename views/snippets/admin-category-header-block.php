7c120a25256882f512217909e6dfeb3b405e2f63
Paul Hughes <paul@paulalexanderhughes.com>
2014-04-16 13:51:16 -0700

    Modified the Custom Navigation header blocks (still non-functional).
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- views/snippets/admin-category-header-block.php
+++ views/snippets/admin-category-header-block.php
@@ -17,10 +17,12 @@ if ( !isset( $header_block_title ) || !is_string( $header_block_title ) ) {
 <li class="muut_forum_header_item" id="category_header_<?php echo $header_block_id; ?>">
 	<div class="muut-category-header-actions">
 		<div class="major-publishing-actions">
-			<label for="header-name-<?php echo $header_block_id; ?>" class="header-input-label"><span><?php _e( 'Header Text', 'muut' ); ?></span></label>
-			<input name="header-title[<?php echo $header_block_id; ?>]" id="header-name-<?php echo $header_block_id; ?>" type="text" class="regular-text menu-item-textbox" title="' . __( 'Header Text', 'muut' ) . '" value="<?php echo $header_block_title; ?>">
+			<label for="header-name-<?php echo $header_block_id; ?>" class="header-input-label"><span><?php _e( 'Header Text:', 'muut' ); ?> </span></label>
+			<input name="header-title[<?php echo $header_block_id; ?>]" id="header-name-<?php echo $header_block_id; ?>" type="text" class="menu-item-textbox" title="<?php _e( 'Header Text', 'muut' ); ?>" value="<?php echo $header_block_title; ?>">
+			<input type="button" class="button button-secondary new_category_for_header" id="new_category_in_<?php echo $header_block_id; ?>" title="<?php _e( 'New Category', 'muut' ); ?>" value="<?php _e( 'New Category', 'muut' ); ?>" />
 		</div>
 	</div>
 	<div class="muut-category-header-content">
+		
 	</div>
 </li>
