009db2212908e876dec3d68f81fd0af1c11b14ad
Paul Hughes <paul@paulalexanderhughes.com>
2014-04-23 15:23:22 -0700

    Added a frontend scripts file and registered in the main muut class.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

---
+++ resources/frontend-functions.js
@@ -0,0 +1,14 @@
+/**
+ * Contains the functionality that will be used for Muut on the frontend
+ * Version 1.0
+ * Requires jQuery
+ *
+ * Copyright (c) 2014 Moot, Inc.
+ * Licensed under MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+jQuery(document).ready( function($) {
+
+  
+
+});
