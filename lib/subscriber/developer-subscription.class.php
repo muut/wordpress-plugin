8d17910fc3f47c24a4a0a01d2d484ae9676a60bd
Paul Hughes <paul@paulalexanderhughes.com>
2014-04-24 15:26:56 -0700

    Initial SSO JS printing done. Still not making the request yet.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- lib/subscriber/developer-subscription.class.php
+++ lib/subscriber/developer-subscription.class.php
@@ -29,6 +29,21 @@ if ( !class_exists( 'Muut_Developer_Subscription' ) ) {
 		protected static $instance;
 
 		/**
+		 * @property string The SSO message.
+		 */
+		private $sso_message;
+
+		/**
+		 * @property string The SSO signature.
+		 */
+		private $sso_signature;
+
+		/**
+		 * @property int The timestamp for SSO.
+		 */
+		private $sso_timestamp;
+
+		/**
 		 * The singleton method.
 		 *
 		 * @return Muut_Developer_Subscription The instance.
@@ -52,6 +67,7 @@ if ( !class_exists( 'Muut_Developer_Subscription' ) ) {
 		protected function __construct() {
 			$this->addActions();
 			$this->addFilters();
+			$this->sso_timestamp = time();
 		}
 
 		/**
@@ -62,7 +78,7 @@ if ( !class_exists( 'Muut_Developer_Subscription' ) ) {
 		 * @since 3.0
 		 */
 		public function addActions() {
-
+			add_action( 'wp_print_scripts', array( $this, 'printSsoJs' ) );
 		}
 
 		/**
@@ -76,5 +92,94 @@ if ( !class_exists( 'Muut_Developer_Subscription' ) ) {
 
 		}
 
+		/**
+		 * Function for generating the message for a given SSO request for the current user.
+		 *
+		 * @return string|false The message encoded, or false on failure (not logged in and whatnot).
+		 * @author Paul Hughes
+		 * @since 3.0
+		 */
+		private function getSsoMessage() {
+			if ( isset( $this->sso_message ) ) {
+				return $this->sso_message;
+			}
+
+			if ( !is_user_logged_in()
+				|| !muut()->getOption( 'subscription_api_key', '' )
+				|| !muut()->getOption( 'subscription_use_sso', '0' )
+			) {
+				return false;
+			}
+
+			$user = wp_get_current_user();
+			$key = muut()->getOption( 'subscription_api_key' );
+			$display_name = $user->display_name;
+			$avatar = get_user_meta($user->ID, 'profilepicture', true);
+			if (!$avatar) {
+				$avatar = function_exists( 'bp_core_fetch_avatar' ) ? bp_core_fetch_avatar( array( 'item_id' => $user->ID, 'html' => false ) ) : "//gravatar.com/avatar/" . md5( $user->user_email );
+			}
+
+			$sso = array(
+				"user" => array(
+					"id" => $user->user_login,
+					"email" => $user->user_email,
+					"avatar" => $avatar,
+					"displayname" => $display_name,
+					"is_admin" => is_super_admin()
+				)
+			);
+
+			$this->sso_message = base64_encode( json_encode( $sso ) );
+			return base64_encode(json_encode($sso));
+		}
+
+		/**
+		 * Get the signature for a given SSO request.
+		 *
+		 * @return string|false The signature for the request, or false on failure (not logged in and whatnot).
+		 * @author Paul Hughes
+		 * @since 3.0
+		 */
+		private function getSsoSignature() {
+			if ( !is_user_logged_in()
+				|| !muut()->getOption( 'subscription_secret_key', '' )
+				|| !muut()->getOption( 'subscription_use_sso', '0' )
+			) {
+				return false;
+			}
+
+			return sha1( muut()->getOption( 'subscription_secret_key', '' ) . ' ' . $this->getSsoMessage() . ' ' . $this->sso_timestamp );
+		}
+
+		/**
+		 * Prints the JS necessary for SSO requests.
+		 *
+		 * @return void
+		 * @author Paul Hughes
+		 * @since 3.0
+		 */
+		public function printSsoJs() {
+			echo '<script type="text/javascript">';
+			if ( is_user_logged_in()
+				&& muut()->getOption( 'subscription_api_key', '' )
+				&& muut()->getOption( 'subscription_use_sso', '0' )
+			) {
+				$key = muut()->getOption( 'subscription_api_key', '' );
+				$timestamp = time();
+
+				echo 'var moot_conf = {';
+				echo 'login_url: "' . wp_login_url( get_permalink() ) . '",';
+				echo 'sso: {';
+				echo 'key: "' .  $key .'",';
+				echo 'timestamp: "' . $this->sso_timestamp . '",';
+				echo 'signature: "' . $this->getSsoSignature() . '",';
+				echo 'message: "' . $this->sso_message . '"';
+				echo '}';
+				echo '}';
+			} else {
+				echo 'var moot_conf = {};';
+			}
+			echo '</script>';
+		}
 	}
 }
