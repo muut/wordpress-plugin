be13f39119250cccbcad8b5448692cc58b09619a
Paul Hughes <paul@paulalexanderhughes.com>
2014-05-13 22:22:25 -0400

    Added enabled/disabled divs to the commenting tab.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- views/blocks/metabox-tab-commenting.php
+++ views/blocks/metabox-tab-commenting.php
@@ -15,10 +15,17 @@ $commenting_defaults = muut()->getOption( 'commenting_defaults' );
 $type = isset( $comments_settings['type'] ) ? $comments_settings['type'] : $commenting_defaults['type'];
 $disable_uploads = isset( $comments_settings['disable_uploads'] ) ? $comments_settings['disable_uploads'] : $commenting_defaults['disable_uploads'];
 ?>
-<p>
-	<span class="muut_metabox_radio"><input type="radio" name="<?php echo $meta_name; ?>[type]" id="muut_comments_type_flat" value="flat" <?php checked( $type, 'flat' ); ?> /><label for="muut_comments_type_flat"><?php _e( 'Flat', 'muut' ); ?></label></span>
-	<span class="muut_metabox_radio"><input type="radio" name="<?php echo $meta_name; ?>[type]" id="muut_comments_type_threaded" value="threaded" class="muut_metabox_radio" <?php checked( $type, 'threaded' ); ?> /><label for="muut_comments_type_threaded"><?php _e( 'Threaded', 'muut' ); ?></label></span>
-</p>
-<p>
-	<span class="checkbox_row"><input type="checkbox" name="<?php echo $meta_name; ?>[disable_uploads]" id="muut_comments_disable_uploads" value="1" <?php checked( $disable_uploads, '1' ); ?> /><label for="muut_comments_disable_uploads"><?php _e( 'Disable image uploads', 'muut' ); ?></label></span>
-</p>
+<div class="enabled_tab_wrapper">
+	<p>
+		<span class="muut_metabox_radio"><input type="radio" name="<?php echo $meta_name; ?>[type]" id="muut_comments_type_flat" value="flat" <?php checked( $type, 'flat' ); ?> /><label for="muut_comments_type_flat"><?php _e( 'Flat', 'muut' ); ?></label></span>
+		<span class="muut_metabox_radio"><input type="radio" name="<?php echo $meta_name; ?>[type]" id="muut_comments_type_threaded" value="threaded" class="muut_metabox_radio" <?php checked( $type, 'threaded' ); ?> /><label for="muut_comments_type_threaded"><?php _e( 'Threaded', 'muut' ); ?></label></span>
+	</p>
+	<p>
+		<span class="checkbox_row"><input type="checkbox" name="<?php echo $meta_name; ?>[disable_uploads]" id="muut_comments_disable_uploads" value="1" <?php checked( $disable_uploads, '1' ); ?> /><label for="muut_comments_disable_uploads"><?php _e( 'Disable image uploads', 'muut' ); ?></label></span>
+	</p>
+</div>
+<div class="disabled_tab_wrapper">
+	<p>
+		<span class="description"><?php printf( __( 'In order to use Muut commenting, you must enable commenting. %sClick here%s to do that now.', 'muut' ), '<a href="#" class="disabled_comments_link">', '</a>' ); ?></span>
+	</p>
+</div>
