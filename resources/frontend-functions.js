341ced1745155f8d51fb1283bfcf27608968d501
Paul Hughes <paul@paulalexanderhughes.com>
2014-05-18 00:51:19 -0400

    Changed where in JS the Comments forum is loaded.
    Also made it so if the comments channel is straight loaded, the m-selected class is applied.
    
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- resources/frontend-functions.js
+++ resources/frontend-functions.js
@@ -13,13 +13,15 @@ jQuery(document).ready( function($) {
   // Adds the comments navigation link to the forum navigation.
   var body = $('body');
   if ( body.hasClass('muut-forum-home') && !body.hasClass('muut-custom-nav') && typeof muut_show_comments_in_nav != 'undefined' && muut_show_comments_in_nav ) {
-    muut().on( 'init', function() {
-      $(".m-forums").append('<p><a href="#!/' + muut_comments_base_domain + '" title="' + muut_frontend_strings.comments + '" data-channel="' + muut_frontend_strings.comments + '"  class="unlisted">' + muut_frontend_strings.comments + '</a></p>');
-    });
     // Make sure the title of the comments page is "Comments".
     muut().on( 'load', function(page) {
+      var comments_link_class = "unlisted ";
       if (typeof( muut_comments_base_domain ) == 'string' && page.relativePath == '/' + muut_comments_base_domain) {
         page.title = "Comments";
+        var comments_link_class = "m-selected";
+      }
+      if ($('#muut_site_comments_nav').length == 0) {
+        $(".m-forums").append('<p><a id="muut_site_comments_nav" href="#!/' + muut_comments_base_domain + '" title="' + muut_frontend_strings.comments + '" data-channel="' + muut_frontend_strings.comments + '"  class="' + comments_link_class + '">' + muut_frontend_strings.comments + '</a></p>');
       }
     });
   }
