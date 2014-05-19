9cb1d0aa51dce96110b9303a4c61f0c0c409e86f
Paul Hughes <paul@paulalexanderhughes.com>
2014-05-19 13:30:36 -0400

    Added first pass at the Muut settings page help tab. NOT real content.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- lib/admin/admin-contextual-help.class.php
+++ lib/admin/admin-contextual-help.class.php
@@ -62,7 +62,7 @@ if ( !class_exists( 'Muut_Admin_Contextual_Help' ) ) {
 		 * @since 3.0
 		 */
 		public function addActions() {
-
+			add_action( 'load-toplevel_page_muut', array( $this, 'addMuutSettingsHelp' ) );
 		}
 
 		/**
@@ -74,6 +74,65 @@ if ( !class_exists( 'Muut_Admin_Contextual_Help' ) ) {
 		 */
 		public function addFilters() {
 
+		}
+
+		/**
+		 * Adds the contextual help tab to the main Muut settings page.
+		 *
+		 * @return void
+		 * @author Paul Hughes
+		 * @since 3.0
+		 */
+		public function addMuutSettingsHelp() {
+			$screen = get_current_screen();
+
+			$help_overview_tab_args = array(
+				'id' => 'muut_settings_help_overview_tab',
+				'title' => __( 'Overview', 'muut' ),
+				'callback' => array( $this, 'renderSettingsHelpOverviewTabContent' ),
+			);
+
+			$help_sso_tab_args = array(
+				'id' => 'muut_settings_help_sso_tab',
+				'title' => __( 'Single Sign-On', 'muut' ),
+				'callback' => array( $this, 'renderSettingsHelpSsoTabContent' ),
+			);
+
+			$screen->add_help_tab( $help_overview_tab_args );
+
+			$screen->add_help_tab( $help_sso_tab_args );
+
+			ob_start();
+				include( muut()->getPluginPath() . 'views/blocks/help-tab-settings-sidebar.php' );
+			$settings_help_sidebar_content = ob_get_clean();
+
+			$screen->set_help_sidebar( $settings_help_sidebar_content );
+		}
+
+		/**
+		 * Renders the content for the help tab on the main Muut settings page.
+		 *
+		 * @param WP_Screen $screen The current screen.
+		 * @param array $tab The current tab array.
+		 * @return void
+		 * @author Paul Hughes
+		 * @since 3.0
+		 */
+		public function renderSettingsHelpOverviewTabContent( $screen, $tab ) {
+			include( muut()->getPluginPath() . 'views/blocks/help-tab-settings-overview.php' );
+		}
+
+		/**
+		 * Renders the content for the help tab on the main Muut settings page.
+		 *
+		 * @param WP_Screen $screen The current screen.
+		 * @param array $tab The current tab array.
+		 * @return void
+		 * @author Paul Hughes
+		 * @since 3.0
+		 */
+		public function renderSettingsHelpSsoTabContent( $screen, $tab ) {
+			include( muut()->getPluginPath() . 'views/blocks/help-tab-settings-sso.php' );
 		}
 	}
 }
---
+++ views/blocks/help-tab-settings-overview.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The content for the Muut settings contextual Overview help tab.
+ *
+ * @package   Muut
+ * @copyright 2014 Muut Inc
+ */
+?>
+<p>
+	We will put some contextual help regarding the Muut settings here.
+</p>
