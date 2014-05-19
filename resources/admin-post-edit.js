b75cf7879bb9355ce91dca002d5e9a787f344c48
Paul Hughes <paul@paulalexanderhughes.com>
2014-05-19 02:58:16 -0400

    Comments metabox disappears (and is disabled altogether) when Muut commenting is enabled.
    The checkbox for it under "Screen Options" also disappears altogether.
    
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- resources/admin-post-edit.js
+++ resources/admin-post-edit.js
@@ -9,6 +9,8 @@
  */
 jQuery(document).ready( function($) {
 
+  var muut_tab_events_object = {};
+
   var muut_post_edit_localized = muut_admin_post_edit_localized;
 
   var muut_toggle_commenting = function() {
@@ -32,7 +34,26 @@ jQuery(document).ready( function($) {
     muut_enable_tab('commenting-tab');
     muut_disable_tab('channel-tab');
     muut_disable_tab('forum-tab');
+    // Hide the comments metabox if Muut commenting is being used.
+    $('#commentsdiv-hide').parent('label').hide();
+    $('#commentsdiv').hide();
   };
+
+  // On page load, if muut commenting is enabled, make sure to hide the comments meta box.
+  if ( $('#muut_enable_tab-commenting-tab').is(':checked')) {
+    $('#commentsdiv-hide').parent('label').hide();
+    $('#commentsdiv').hide();
+  }
+
+  // When commenting is disabled, make sure to show the Commenting metabox/check box (if checked).
+  $(muut_tab_events_object).on('tab_disable', function ( event, tab_name ) {
+    if ( tab_name == 'commenting-tab' ) {
+      $('#commentsdiv-hide').parent('label').show();
+      if ($('#commentsdiv-hide').is(':checked')) {
+        $('#commentsdiv').show();
+      }
+    }
+  });
 
   var muut_enable_channel = function() {
     $('#comment_status').prop('checked', false);
@@ -88,12 +109,14 @@ jQuery(document).ready( function($) {
     $('#muut_tab_content-' + tab_name + ', li[data-muut_tab=' + tab_name +']').addClass('disabled').removeClass('enabled');
     $('#muut_enable_tab-' + tab_name).prop('checked', false);
     $('.muut_tab_last_active[name=muut_tab_last_active_' + tab_name +']').val(0);
+    $(muut_tab_events_object).trigger('tab_disable', [ tab_name ]);
   };
 
   var muut_enable_tab = function( tab_name ) {
     $('#muut_tab_content-' + tab_name + ', li[data-muut_tab=' + tab_name +']').addClass('enabled').removeClass('disabled');
     $('#muut_enable_tab-' + tab_name).prop('checked', true);
     $('.muut_tab_last_active[name=muut_tab_last_active_' + tab_name +']').val(1);
+    $(muut_tab_events_object).trigger('tab_enable', [ tab_name ]);
   };
 
   if ($.fn.tabs) {
