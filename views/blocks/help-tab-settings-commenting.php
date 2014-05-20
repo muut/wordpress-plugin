3e67573dd2f97679c057e83c306d4d2ace617902
Paul Hughes <paul@paulalexanderhughes.com>
2014-05-20 10:34:42 -0400

    Added content for Settings help tab (minus SSO). i18ned
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- views/blocks/help-tab-settings-commenting.php
+++ views/blocks/help-tab-settings-commenting.php
@@ -6,6 +6,13 @@
  * @copyright 2014 Muut Inc
  */
 ?>
-<p>
-	We will put some contextual help regarding the Commenting Muut settings here. Everything in that second grouping on this settings page.
-</p>
+<ul>
+	<li>
+		<p><strong><?php _e( 'Use Muut for post commenting', 'muut' ); ?></strong>
+			<?php _e( 'Replaces default Wordpress commenting.', 'muut' ); ?></p>
+	</li>
+	<li>
+		<p><strong><?php _e( 'Use Muut commenting on posts with existing comments', 'muut' ); ?></strong>
+			<?php printf( __( 'Posts that already have comments using the default WordPress discussion system will not default to using Muut. This can be changed on a post-by-post bases using the %sCommenting%s tab in the Muut metabox for that post.', 'muut' ), '<em>', '</em>' ); ?></p>
+	</li>
+</ul>
--- views/blocks/help-tab-settings-overview.php
+++ views/blocks/help-tab-settings-overview.php
@@ -6,6 +6,5 @@
  * @copyright 2014 Muut Inc
  */
 ?>
-<p>
-	We will put some contextual help regarding the Muut settings here.
-</p>
+<p><?php printf( __( 'If you have already have a Muut forum, you can enter your forum name on the settings page right away. If not, you\'ll have to set up a %snew forum%s at the Muut website first.', 'muut' ), '<a href="https://muut.com/setup/">', '</a>' ); ?></p>
+<p><?php _e( 'Once you have saved your forum, you can edit the other various settings for your implementation and you can go ahead and add a new page.', 'muut' ); ?></p>
