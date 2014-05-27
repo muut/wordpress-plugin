9eee35ed435fbc306d7967029a06d0dc8d119c07
Paul Hughes <paul@paulalexanderhughes.com>
2014-05-27 16:52:52 -0400

    Set up base unit tests directory.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

---
+++ tests/test-baseFalseIsFalseassertion.php
@@ -0,0 +1,17 @@
+<?php
+ 
+/**
+ * An example test case.
+ */
+class MyPlugin_Test_Example extends WP_UnitTestCase {
+ 
+    /**
+     * An example test.
+     *
+     * We just want to make sure that false is still false.
+     */
+    function test_false_is_false() {
+ 
+        $this->assertFalse( false );
+    }
+}
