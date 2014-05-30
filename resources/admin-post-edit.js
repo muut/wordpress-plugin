24f45a8c7d873bbe5d25d1399dd90f99778d4306
Paul Hughes <paul@paulalexanderhughes.com>
2014-05-30 09:14:41 -0400

    Optimized JS for post editor.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>
    See https://trello.com/c/UlE9SQ9e
    
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- resources/admin-post-edit.js
+++ resources/admin-post-edit.js
@@ -9,26 +9,48 @@
  */
 jQuery(document).ready( function($) {
 
-  var muut_tab_events_object = {};
-
+  // Set the localization variable containing the localized strings.
   var muut_post_edit_localized = muut_admin_post_edit_localized;
 
+  // Set up the tab-events-object which will some of the event listening to our custom triggers.
+  var muut_tab_events_object = {};
+
+  // Define the function to toggle Muut commenting.
+  // Decides what to do based on the main WordPress discussion meta box "allow commenting" option.
   var muut_toggle_commenting = function() {
     var comment_status_checkbox = $('#comment_status');
     if (comment_status_checkbox.is(':checked')) {
       muut_enable_commenting();
       $('.muut_tab_last_active[name=muut_tab_last_active_commenting-tab]').val(1);
       muut_set_current_tab( 'commenting-tab' );
-    }
-    if (!comment_status_checkbox.is(':checked')) {
+    } else {
       muut_disable_tab('commenting-tab');
     }
   };
 
+  // Generalized function to set which tab is currently showing in the Muut meta box.
   var muut_set_current_tab = function( tab_name ) {
     $('#muut_tab-' + tab_name + ' a.muut_metabox_tab').click();
   };
 
+  // On page load, if muut commenting is enabled, make sure to hide the comments meta box.
+  if ( $('#muut_enable_tab-commenting-tab').is(':checked')) {
+    $('#commentsdiv-hide').parent('label').hide();
+    $('#commentsdiv').hide();
+  }
+
+  // When Muut commenting is disabled, make sure to show the Commenting metabox/check box (if checked).
+  $(muut_tab_events_object).on('tab_disable', function ( event, tab_name ) {
+    if ( tab_name == 'commenting-tab' ) {
+      var default_wp_commenting_metabox_display_element = $('#commentsdiv-hide');
+      default_wp_commenting_metabox_display_element.parent('label').show();
+      if (default_wp_commenting_metabox_display_element.is(':checked')) {
+        $('#commentsdiv').show();
+      }
+    }
+  });
+
+  // Declare function to enable Muut commenting.
   var muut_enable_commenting = function() {
     $('#comment_status').prop('checked', true);
     muut_enable_tab('commenting-tab');
@@ -39,22 +61,7 @@ jQuery(document).ready( function($) {
     $('#commentsdiv').hide();
   };
 
-  // On page load, if muut commenting is enabled, make sure to hide the comments meta box.
-  if ( $('#muut_enable_tab-commenting-tab').is(':checked')) {
-    $('#commentsdiv-hide').parent('label').hide();
-    $('#commentsdiv').hide();
-  }
-
-  // When commenting is disabled, make sure to show the Commenting metabox/check box (if checked).
-  $(muut_tab_events_object).on('tab_disable', function ( event, tab_name ) {
-    if ( tab_name == 'commenting-tab' ) {
-      $('#commentsdiv-hide').parent('label').show();
-      if ($('#commentsdiv-hide').is(':checked')) {
-        $('#commentsdiv').show();
-      }
-    }
-  });
-
+  // Declare function to enable Muut channel embed.
   var muut_enable_channel = function() {
     $('#comment_status').prop('checked', false);
     muut_disable_tab('commenting-tab');
@@ -62,6 +69,7 @@ jQuery(document).ready( function($) {
     muut_disable_tab('forum-tab');
   };
 
+  // Declare function to enable Muut forum page embed.
   var muut_enable_forum = function() {
     $('#comment_status').prop('checked', false);
     muut_disable_tab('commenting-tab');
@@ -69,10 +77,12 @@ jQuery(document).ready( function($) {
     muut_enable_tab('forum-tab');
   };
 
+  // When the WP "Allow Comments" checkbox state changes, toggle the Muut commenting enabled status.
   $('#comment_status').on('change', function() {
     muut_toggle_commenting();
   });
 
+  // Make sure proper functionality is used when Muut commenting activation checkbox changes.
   $('input.muut_enable_commenting-tab').on('change', function(e) {
     if ( $(this).is(':checked') ) {
       muut_enable_commenting();
@@ -82,6 +92,7 @@ jQuery(document).ready( function($) {
     e.preventDefault();
   });
 
+  // Make sure proper functionality is used when Muut channel embed activation checkbox changes.
   $('input.muut_enable_channel-tab').on('change', function(e) {
     if ( $(this).is(':checked') ) {
       muut_enable_channel();
@@ -91,6 +102,7 @@ jQuery(document).ready( function($) {
     e.preventDefault();
   });
 
+  // Make sure proper functionality is used when Muut forum page embed activation checkbox changes.
   $('input.muut_enable_forum-tab').on('change', function(e) {
     if ( $(this).is(':checked') ) {
       muut_enable_forum();
@@ -100,11 +112,7 @@ jQuery(document).ready( function($) {
     e.preventDefault();
   });
 
-  $('a.disable_muut_commenting_link').on('click', function(e) {
-    muut_disable_tab('commenting-tab');
-    e.preventDefault();
-  });
-
+  // Generalized function to disable a specific tab.
   var muut_disable_tab = function( tab_name ) {
     $('#muut_tab_content-' + tab_name + ', li[data-muut_tab=' + tab_name +']').addClass('disabled').removeClass('enabled');
     $('#muut_enable_tab-' + tab_name).prop('checked', false);
@@ -112,6 +120,7 @@ jQuery(document).ready( function($) {
     $(muut_tab_events_object).trigger('tab_disable', [ tab_name ]);
   };
 
+  // Generalized function to enable a specific tab.
   var muut_enable_tab = function( tab_name ) {
     $('#muut_tab_content-' + tab_name + ', li[data-muut_tab=' + tab_name +']').addClass('enabled').removeClass('disabled');
     $('#muut_enable_tab-' + tab_name).prop('checked', true);
@@ -119,16 +128,19 @@ jQuery(document).ready( function($) {
     $(muut_tab_events_object).trigger('tab_enable', [ tab_name ]);
   };
 
+  // Check if jQuery Tabs UI API is enabled before executing related functionality.
   if ($.fn.tabs) {
-    $('#muut_metabox_tabs .muut-tab-panel.hidden').removeClass('hidden');
-    $('#muut_metabox_tabs').tabs({
+    var muut_metabox_tabs_element = $('#muut_metabox_tabs');
+    muut_metabox_tabs_element.find('.muut-tab-panel.hidden').removeClass('hidden');
+    // Execute the Tabs functionality for the Muut metabox tabs.
+    muut_metabox_tabs_element.tabs({
       activate: function(event, ui){
         ui.newTab.addClass('tabs');
         ui.newTab.children('input.muut_tab_last_open').val('1');
         ui.oldTab.removeClass('tabs');
         ui.oldTab.children('input.muut_tab_last_open').val('0');
       },
-      active: $('#muut_metabox_tabs_list li.tabs').index()
+      active: $('#muut_metabox_tabs_list').find('li.tabs').index()
     });
   }
 });
