0dbd2bf90cd4ceaa41f83f46ba3406687b339af6
Paul Hughes <paul@paulalexanderhughes.com>
2014-05-31 15:31:18 -0400

    Added the widget library directory and base/empty class for the channel embed widget.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

---
+++ lib/widgets/widget-channel-embed.class.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * The Individual Channel Embed widget.
+ *
+ * @package   Muut
+ * @copyright 2014 Muut Inc
+ */
+
+// Don't load directly
+if ( !defined( 'ABSPATH' ) ) {
+	die( '-1' );
+}
+
+if ( !class_exists( 'Muut_Widget_Channel_Embed' ) ) {
+	/**
+	 * Muut Channel Embed widget class.
+	 *
+	 * @package Muut
+	 * @author  Paul Hughes
+	 * @since   NEXT_RELEASE
+	 */
+	class Muut_Widget_Channel_Embed extends WP_Widget {
+
+		/**
+		 * The class constructor.
+		 *
+		 * @return Muut_Widget_Channel_Embed
+		 * @author Paul Hughes
+		 * @since NEXT_RELEASE
+		 */
+		function __construct() {
+			// Constructor stuff.
+		}
+
+		/**
+		 * Render the widget frontend output.
+		 *
+		 * @param array $args The sidebar arguments.
+		 * @param array $instance The widget instance parameters.
+		 * @return void
+		 * @author Paul Hughes
+		 * @since NEXT_RELEASE
+		 */
+		public function widget( $args, $instance ) {
+			// Output the frontend widget content.
+		}
+
+		/**
+		 * Render the admin form for widget customization.
+		 *
+		 * @param array $instance The widget instance parameters.
+		 * @return void
+		 * @author Paul Hughes
+		 * @since NEXT_RELEASE
+		 */
+		public function form( $instance ) {
+			// Output the widget admin form for a given instance's customization.
+		}
+
+		/**
+		 * Process the widget arguments to save the customization for that instance.
+		 *
+		 * @param array $new_instance The changed/new arguments.
+		 * @param array $old_instance The previous/old arguments.
+		 * @return void
+		 * @author Paul Hughes
+		 * @since NEXT_RELEASE
+		 */
+		public function update( $new_instance, $old_instance ) {
+			// Save the widget data.
+		}
+	}
+}
