001dbfd4bcd88bda13d58360c3fbfff501bdfe2f
Paul Hughes <paul@paulalexanderhughes.com>
2014-07-18 16:07:43 -0400

    Facelinks only initialized once per page-load.
    Signed-off-by: Paul Hughes <paul@paulalexanderhughes.com>

--- views/widgets/widget-latest-comments.php
+++ views/widgets/widget-latest-comments.php
@@ -29,7 +29,7 @@
 				$list_time = floor( $time_since / ( 60 * 60 * 24 * 7 ) ) . 'w';
 			}
 			$user = $comment['user'];
-			$user_link_path = Muut_Post_Utility::getForumPageId() ? get_permalink( Muut_Post_Utility::getForumPageId() ) . '#!/' . $user->path . '"' : false;
+			$user_link_path = Muut_Post_Utility::getForumPageId() && Muut_Post_Utility::getForumPageId() != get_the_ID() ? get_permalink( Muut_Post_Utility::getForumPageId() ) . '#!/' . $user->path . '"' : false;
 			echo '<li class="muut_recentcomments">';
 				muut_get_user_facelink_avatar( substr( $user->path, 1 ), $user->displayname, false, $user_link_path, $user->img, true );
 				echo '<span class="recent-comments-post-title"><a href="' . get_permalink( $comment['post_id'] ) . '">' . get_the_title( $comment['post_id'] ) . '</a></span>';
